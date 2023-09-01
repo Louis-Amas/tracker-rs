a6319bdbddf232d8bee40edf81c8be8d8f891c8c
Louis-Amas <amaslouis@protonmail.com>
2023-09-01 16:32:25 +0200

    feat: add types

--- src/tracker/tracker.rs
+++ src/tracker/tracker.rs
@@ -1,7 +1,70 @@
-use crate::types::block::Block;
-use ethers::core::types::TxHash;
+#![allow(dead_code)]
+use tracing::{event, Level};
+use std::collections::HashMap;
 
-pub struct ChainCache {
-   
-    pub last_block: Block<TxHash>,
+use crate::types::{block::Block, self};
+use ethers::{core::types::TxHash, types::{U64, H256, ValueOrArray, Address, Topic, Log}};
+
+pub struct ChainCacheOptions {
+    pub max_block_cached: u32,
+
+    pub retries_count: u32,
+
+    pub retry_delay_ms: u32,
+
+    pub batch_size: u32,
+}
+
+pub struct BlockCache<Provider: RpcProvider<TxHash>> {
+    pub rpc_provider: Provider,
+
+    pub options: ChainCacheOptions,
+
+    pub last_block: Option<Block<TxHash>>,
+
+    pub blocks_map: HashMap<U64, Block<TxHash>>,
+}
+
+pub enum BlockIdentifier {
+    Latest,
+    Number(U64),
+    Hash(H256),
+}
+
+pub enum FilterBlockOption {
+    Range {
+        from_block: BlockIdentifier,
+        to_block: BlockIdentifier,
+    },
+    AtBlock(BlockIdentifier),
+}
+
+pub trait RpcProvider<TX> {
+    fn get_block(&self, number: BlockIdentifier) -> Result<types::block::Block<TX>, String>;
+
+    fn get_logs(&self, filter: Filter) -> Result<Vec<Log>, String>;
+}
+
+pub struct Filter {
+    pub block_option: FilterBlockOption,
+
+    pub address: Option<ValueOrArray<Address>>,
+
+    pub topics: [Option<Topic>; 4],
+}
+
+impl<Provider: RpcProvider<TxHash>> BlockCache<Provider> {
+    fn add_block(&mut self, block: Block<TxHash>) {
+        event!(Level::INFO, "add_block {}", block);
+        self.last_block = Some(block);
+    }
+
+    fn initialize(&mut self, block: Block<TxHash>) {
+
+        self.add_block(block);
+    }
+
+    fn handle_block(&mut self, _block: Block<TxHash>) {
+
+    }
 }
--- src/types/block.rs
+++ src/types/block.rs
@@ -1,4 +1,6 @@
-use ethers::{core::types::H256, types::{U64, Bloom, H64}};
+use core::fmt;
+
+use ethers::{core::types::{H256, TxHash}, types::{U64, Bloom, H64}};
 
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct Block<TX> {
@@ -14,4 +16,11 @@ pub struct Block<TX> {
     pub nonce: H64, 
 
     pub logs_bloom: Bloom,
+}
+
+impl fmt::Display for Block<TxHash> {
+
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "(hash: {}, parent_hash: {}, number: {}, transactions_length: {}, nonce: {}, logs_bloom: {})", self.hash, self.parent_hash, self.number, self.transactions.len(), self.nonce, self.logs_bloom)
+    }
 }
