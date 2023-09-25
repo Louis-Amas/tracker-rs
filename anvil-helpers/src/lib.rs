7b520782235e6a5424b53423429c7fe512d1808a
Louis-Amas <amaslouis@protonmail.com>
2023-09-25 17:17:16 +0200

    fix(anvil): implement deploy a contract

--- anvil-helpers/src/lib.rs
+++ anvil-helpers/src/lib.rs
@@ -1,9 +1,11 @@
 pub mod anvil_helpers {
+    use std::collections::HashMap;
     use std::sync::mpsc::{self, Sender};
 
     use std::process::{Command, ExitStatus, Stdio};
-    use std::thread::{self, JoinHandle, sleep};
+    use std::thread::{self, sleep, JoinHandle};
 
+    use regex::Regex;
     use std::net::TcpStream;
     use std::time::Duration;
     use tracing::{event, Level};
@@ -13,11 +15,17 @@ pub mod anvil_helpers {
         pub private_key: &'static str,
     }
 
+    pub struct ContractInfo {
+        pub path: String,
+        pub name: String,
+    }
+
     pub struct Anvil {
         pub port: u16,
         pub accounts: [Account; 6],
         thread: JoinHandle<Result<ExitStatus, String>>,
         sender_parent_to_child: Sender<bool>,
+        pub contracts_map: HashMap<String, String>,
     }
 
     pub fn is_port_open(host: &str, port: u16) -> bool {
@@ -55,7 +63,33 @@ pub mod anvil_helpers {
     ];
 
     impl Anvil {
-        pub fn new(port: Option<u16>, block_time_s: Option<u32>) -> Self {
+        pub fn new_contract(&self, contract: &ContractInfo) -> Result<String, String> {
+            let mut cmd = Command::new("forge");
+
+            cmd.current_dir("./contracts/");
+            cmd.arg("create");
+            cmd.arg("--rpc-url");
+            cmd.arg(format!("http://127.0.0.1:{}", self.port));
+            cmd.arg(format!("{}:{}", contract.path, contract.name));
+            cmd.arg("--private-key");
+            cmd.arg(self.accounts[0].private_key);
+
+            let output = Stdio::piped();
+            cmd.stdout(output);
+
+            let child = cmd.spawn().map_err(|err| err.to_string())?;
+
+            let output = child.wait_with_output().map_err(|err| err.to_string())?;
+            let regex = Regex::new(r"Deployed to: (0x[0-9a-fA-F]*)").unwrap();
+            let stdout_str = String::from_utf8(output.stdout).unwrap();
+
+            event!(Level::DEBUG, stdout_str);
+            let capture = regex.captures(&stdout_str).unwrap();
+
+            Ok(capture.get(1).unwrap().as_str().to_string())
+        }
+
+        pub fn new(port: Option<u16>, block_time_s: Option<u32>, contracts: Vec<ContractInfo>) -> Self {
             event!(Level::DEBUG, "New anvil");
 
             let port = port.unwrap_or(8545);
@@ -87,46 +121,87 @@ pub mod anvil_helpers {
                             sleep(Duration::from_millis(500));
                         }
 
-                        sender_child_to_parent.send(true).map_err(|err| err.to_string())?;
-                        receiver_parent_to_child.recv().map_err(|err| err.to_string())?;
+                        sender_child_to_parent
+                            .send(true)
+                            .map_err(|err| err.to_string())?;
+                        receiver_parent_to_child
+                            .recv()
+                            .map_err(|err| err.to_string())?;
                         child.kill().map_err(|err| err.to_string())?;
                         Ok(child.wait().map_err(|err| err.to_string())?)
-                    },
+                    }
                 }
             });
 
             receiver_child_to_parent.recv().unwrap();
 
-            Anvil {
+            let mut anvil = Anvil {
                 port,
                 accounts: ACCOUNTS,
                 thread,
                 sender_parent_to_child,
+                contracts_map: HashMap::new(),
+            };
+
+            if block_time_s.is_none() && contracts.len() > 0 {
+                event!(Level::ERROR, "Could not initialize contract as there is no block-time");
+                return anvil;
             }
+
+            for contract in contracts.into_iter() {
+                let addr = anvil.new_contract(&contract).unwrap();
+
+                anvil.contracts_map.insert(contract.name, addr);
+            } 
+
+            anvil
         }
 
         pub fn kill(self) {
             self.sender_parent_to_child.send(true).unwrap();
 
             self.thread.join().unwrap().unwrap();
-        } 
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::anvil_helpers::{Anvil, is_port_open};
+    use crate::anvil_helpers::ContractInfo;
+
+    use super::anvil_helpers::{is_port_open, Anvil};
 
     use tracing_test::traced_test;
 
     #[test]
     #[traced_test]
     fn test() {
-        let anvil = Anvil::new(None, None);
+        let anvil = Anvil::new(None, Some(2), Vec::new());
 
         assert_eq!(anvil.port, 8545);
 
         assert_eq!(is_port_open("localhost", anvil.port), true);
+
+        anvil.kill();
+    }
+
+    #[test]
+    #[traced_test]
+    fn test_deploy_multicall2() {
+        let anvil = Anvil::new(None, Some(2), vec![
+            ContractInfo {
+                path: "src/Multicall2.sol".to_string(),
+                name: "Multicall2".to_string(),
+            }]
+        );
+
+        assert_eq!(anvil.port, 8545);
+
+        assert_eq!(is_port_open("localhost", anvil.port), true);
+
+        let addr = anvil.contracts_map.get(&"Multicall2".to_string());
+
+        assert_eq!(addr.is_some(), true);
 
         anvil.kill();
     }
